b31d6c7d7e1c6ce4abdda74990e454a50cafaa08
roberto-barros <jroberto.barros2506@gmail.com>
2021-11-25 20:18:28 -0300

    add slider

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import SwiperCore, {Pagination} from 'swiper';
+import { Swiper, SwiperSlide} from 'swiper/react';
+import Card from "../Card";
+import 'swiper/swiper-bundle.css'
+
+SwiperCore.use(Pagination);
+
+const Slider = props => {
+
+    return (
+        <Swiper slidesPerView={4}>
+            <SwiperSlide>
+                <Card/>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Card/>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Card/>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Card/>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Card/>            
+            </SwiperSlide>
+        </Swiper>
+    )
+}
+
+export default Slider;
